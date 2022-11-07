6a01628eb6850a5d65c4cfd9ba547ef87b5bdcf7
emmacodes <binma.emma@gmail.com>
2022-11-06 23:35:36 -0800

    complete displaying menu items

--- index.js
+++ index.js
@@ -1,3 +1,52 @@
 import { menuArray } from "/data.js"
 const menuContent = document.getElementById('menu-content')
 
+function getMenu(){
+    let menuHtml = ""
+    menuArray.forEach(function(menu){
+        menuHtml +=  `
+        <div class="order-container">
+            <div class="order-detail-left">
+        
+                <div class="emoji">
+                    ${menu.emoji}
+                </div>
+        
+        
+                 <div class="item-details">
+        
+                    <div class="item-name">
+                        ${menu.name}
+                    </div>
+        
+                    <div class="item-ingredients">
+                        ${menu.ingredients}
+                    </div>
+        
+                    <div class="item-price">
+                       $ ${menu.price}
+                    </div>
+        
+                 </div>
+            </div>
+        
+            <div class="order-add-btn">
+            <i class="fa fa-light fa-circle-plus fa-4x"></i>
+            </div>
+        </div>
+        `
+
+
+    })
+    return menuHtml
+
+}
+
+
+
+
+function render(){
+    menuContent.innerHTML = getMenu()
+}
+
+render()
